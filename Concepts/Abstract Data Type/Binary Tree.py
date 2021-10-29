1d25bccd2718b1a5c9de6fa9f52f5d900660b702
Ternt <45267060+Ternt@users.noreply.github.com>
2021-10-29 23:12:57 +0700

    Delete Concepts directory

--- Concepts/Abstract Data Type/Binary Tree.py
+++
@@ -1,9 +0,0 @@
-class Node:
-    def __init__(self, leftPointer, rightpointer, data):
-        sef.data = data
-        self.leftPointer = leftPointer
-        self.rightPointer = rightPointer
-
-class BinaryTree:
-    def __init__(self, size):
-        
