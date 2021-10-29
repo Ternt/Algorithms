5b3e1e2107152485bbc7e502abfe06474b813682
Ternt <45267060+Ternt@users.noreply.github.com>
2021-10-29 23:11:13 +0700

    Delete Projects directory

--- Projects/Simple Randomizer.py
+++
@@ -1,6 +0,0 @@
-import random
-
-List = [0,1]
-
-def printrandomly():
-    print(List[random.randint(0,1)])
--- Projects/bikes.py
+++
@@ -1,7 +0,0 @@
-wheels = 0
-frame = 0
-link = 0
-
-wheels = int(input("how many wheels u got homie?: "))
-wheels = wheels/2
-if 
